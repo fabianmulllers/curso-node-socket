f2b6b8bc0e93fa1567d954de037d3cc72caaca2c
Fabian Muller <fabian.muller.saavedra@gmail.com>
2021-07-19 15:13:14 -0400

    se inicia git

---
+++ sockets/controller.js
@@ -0,0 +1,72 @@
+const TicketControl = require("../models/ticket-control");
+
+const ticketControl = new TicketControl();
+
+
+const socketController = (socket) => {
+    
+
+    socket.on('disconnect', () => {});
+    //cuando cliente se conecta
+    socket.emit('estado-actual', ticketControl.ultimos4);
+    socket.emit('ultimo-ticket', ticketControl.ultimo );
+    socket.emit('ticket-pendientes',ticketControl.tickets.length);
+    
+
+    socket.on('siguiente-ticket', ( payload, callback ) => {
+
+        const siguiente = ticketControl.siguiente();
+
+        socket.broadcast.emit('ticket-pendientes',ticketControl.tickets.length);
+
+        callback(siguiente);
+
+        //TODO: notificar que hay un nuevo ticket pendiente de asignar
+
+        
+    });
+
+
+    socket.on('atender-ticket',( { escritorio }, callback ) => { 
+
+        if(!escritorio){
+
+            return callback({
+                ok:false,
+                msg:'El escritorio es obligatorio'
+            });
+        }
+        
+        const ticket = ticketControl.atenderTicket( escritorio );
+        //TODO:notificar cambio en los ultimos4
+
+        socket.broadcast.emit('estado-actual', ticketControl.ultimos4);
+
+        socket.emit('ticket-pendientes',ticketControl.tickets.length);
+
+        socket.broadcast.emit('ticket-pendientes',ticketControl.tickets.length);
+
+        
+        if( ! ticket ){
+            callback({
+                ok:false,
+                msg:'ya no hay tickets pendientes'
+            })
+        }else{
+            callback({
+                ok:true,
+                ticket
+            })
+        }
+
+    });
+
+
+}
+
+
+
+module.exports = {
+    socketController
+}
+
